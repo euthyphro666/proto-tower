fa0db306cf37cc50d6907dd3a401811691272ac0
Josh <joshhess13@gmail.com>
2020-02-26 20:53:58 -0700

    added nav mesh for enemy movement to any waypoint in the scene

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyPathing : MonoBehaviour
+{
+    public void Awake()
+    {
+        var agent = GetComponent<NavMeshAgent>();
+        var destination = GameObject.FindGameObjectWithTag("Waypoint");
+        if (destination != null)
+            agent.SetDestination(destination.transform.position);
+        else
+            Debug.LogWarning($"[ Enemy Pathing ] Unable to find waypoint to move to.");
+    }
+}
